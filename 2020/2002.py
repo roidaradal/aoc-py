6b4b88c559c1304a413bb56169c418d91027cde8
roidaradal <johnroy.daradal@goodkredit.com>
2025-05-18 20:12:56 +0800

    20.02 Py

---
+++ 2020/2002.py
@@ -0,0 +1,50 @@
+# Advent of Code 2020 Day 02
+# John Roy Daradal 
+
+from aoc import *
+
+class Password:
+    def __init__(self, line: str):
+        head, tail = splitStr(line, ':')
+        p = head.split()
+        self.num1, self.num2 = toIntList(p[0], '-')
+        self.char = p[1]
+        self.text = tail
+
+def data(full: bool) -> list[Password]:
+    return [Password(line) for line in readLines(20, 2, full)]
+
+def part1():
+    passwords = data(full=True)
+    count = countValid(passwords, isValid)
+    print(count) 
+
+def part2():
+    passwords = data(full=True)
+    count = countValid(passwords, isValid2)
+    print(count) 
+
+def isValid(p: Password) -> bool:
+    freq = charFreq(p.text)
+    return p.num1 <= freq[p.char] <= p.num2
+
+def isValid2(p: Password) -> bool:
+    count = 0 
+    for idx in [p.num1-1, p.num2-1]:
+        if p.text[idx] == p.char:
+            count += 1
+    return count == 1
+
+
+if __name__ == '__main__':
+    do(part1)
+    do(part2)
+
+'''
+Part1:
+- Valid if frequency of char is within range of num1, num2 inclusive
+
+Part2:
+- Valid if exactly 1 of the num1, num2 as the index has char
+- Subtract 1 from num1, num2 because it is using 1-index
+'''
