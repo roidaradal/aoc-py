30f9ce82c7a3014201d0105ecabc50f70b8b89ed
roidaradal <johnroy.daradal@goodkredit.com>
2025-05-18 19:08:35 +0800

    18.02 Py

---
+++ 2018/1802.py
@@ -0,0 +1,52 @@
+# Advent of Code 2018 Day 02
+# John Roy Daradal 
+
+import itertools
+from aoc import *
+
+def data(full: bool) -> list[str]:
+    return readLines(18, 2, full)
+
+def part1():
+    words = data(full=True)
+    count2, count3 = 0, 0 
+    for word in words:
+        freq = charFreq(word).values()
+        if 2 in freq: count2 += 1 
+        if 3 in freq: count3 += 1
+    print(count2 * count3) 
+
+def part2():
+    words = data(full=True)
+    for word1, word2 in itertools.combinations(words, 2):
+        diff = strDiff(word1, word2)
+        if len(diff) != 1: continue 
+
+        idx = diff[0]
+        word = word1[:idx] + word1[idx+1:]
+        print(word)
+        break
+
+# Return list of index where word1 and word2 differs
+# Assumes word1 and word2 have same length
+def strDiff(word1: str, word2: str) -> list[int]:
+    diff = []
+    for i in range(len(word1)):
+        if word1[i] != word2[i]:
+            diff.append(i)
+    return diff
+
+if __name__ == '__main__':
+    do(part1)
+    do(part2)
+
+'''
+Part1:
+- Get character frequency values of each word
+- Increase count2 if has freq of 2
+- Increase count3 if has freq of 3 
+
+Part2:
+- Loop through pair combinations of words 
+- If strDiff is only 1, remove char at that index 
+'''
