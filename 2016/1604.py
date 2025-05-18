8f6406e015d701cb2b99c4f0d67c3a549f2946e1
roidaradal <johnroy.daradal@goodkredit.com>
2025-05-18 13:13:03 +0800

    16.04 Py

---
+++ 2016/1604.py
@@ -0,0 +1,67 @@
+# Advent of Code 2016 Day 04
+# John Roy Daradal 
+
+from aoc import * 
+
+class Room:
+    def __init__(self, line: str):
+        head, tail = line.split('[')
+        p = head.split('-')
+        self.checksum = tail.strip(']')
+        self.name = '-'.join(p[:-1])
+        self.id = int(p[-1])
+    
+    @property 
+    def isReal(self) -> bool:
+        freq = charFreq(self.name, skip=['-'])
+        freq = sorted((-v,k) for k,v in freq.items())
+        if len(freq) < 5: 
+            return False 
+        top5 = ''.join(x for _,x in freq[:5])
+        return top5 == self.checksum
+    
+    def decrypt(self) -> str: 
+        msg = list(self.name)
+        for _ in range(self.id):
+            msg = [rotate(letter) for letter in msg]
+        return ''.join(msg)
+
+def data(full: bool) -> list[Room]:
+    return [Room(line) for line in readLines(16, 4, full)]
+
+def part1():
+    rooms = data(full=True)
+    fn = lambda room: room.id if room.isReal else 0 
+    total = getTotal(rooms, fn)
+    print(total) 
+
+def part2():
+    rooms = data(full=True)
+    goal = 'northpole-object-storage'
+    for room in rooms:
+        if room.decrypt() == goal:
+            print(room.id)
+            break
+
+def rotate(letter: str) -> str:
+    if letter == 'z':
+        return 'a'
+    elif letter == '-':
+        return '-'
+    return chr(ord(letter)+1)
+
+if __name__ == '__main__':
+    do(part1)
+    do(part2)
+
+'''
+Part1:
+- Loop through each room, sum up roomID of valid rooms 
+- Count frequency of letters in name (except -)
+- Get top5 most frequent letters and check if same as checksum 
+
+Part2:
+- Loop through each room and decrypt room name to find 'northpole-object-storage'
+- Repeat roomID times: rotate each letter in the name 
+- Rotate letter: next letter with wraparound (Z => A); dont translate '-'
+'''
