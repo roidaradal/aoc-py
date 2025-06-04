1ca21e16beccfcb79fa34a6e19e3dddff301f591
roidaradal <johnroy.daradal@goodkredit.com>
2025-06-04 19:23:30 +0800

    17.15 Py

---
+++ 2017/1715.py
@@ -0,0 +1,67 @@
+# Advent of Code 2017 Day 15
+# John Roy Daradal 
+
+from aoc import *
+
+def data(full: bool) -> int2:
+    line1, line2 = readLines(17, 15, full)
+    a = int(line1.split()[-1])
+    b = int(line2.split()[-1])
+    return a, b
+
+def part1():
+    prevAB = data(full=True)
+    count = solve(prevAB, (0, 0), 40_000_000)
+    print(count)
+
+def part2():
+    prevAB = data(full=True)
+    count = solve(prevAB, (4, 8), 5_000_000)
+    print(count)
+
+def solve(prevAB: int2, multAB: int2, numSteps: int) -> int:
+    prevA, prevB = prevAB 
+    multA, multB = multAB
+    factorA, factorB = 16807, 48271 
+    
+    genA = generator(prevA, factorA, multA)
+    genB = generator(prevB, factorB, multB)
+    count = 0 
+    factor = 1_000_000
+    for i in range(numSteps):
+        if i % factor == 0: print(i // factor)
+        a = next(genA)
+        b = next(genB)
+        if isMatch(a, b): count += 1
+    return count
+
+def generator(prev: int, factor: int, mult: int):
+    magic = 2147483647
+    while True:
+        curr = (prev * factor) % magic
+        prev = curr 
+        if mult == 0 or curr % mult == 0:
+            yield curr 
+
+def isMatch(a: int, b: int) -> bool:
+    x = binaryFilled(a, 16)
+    y = binaryFilled(b, 16)
+    return x[-16:] == y[-16:]
+
+if __name__ == '__main__':
+    do(part1)
+    do(part2)
+
+'''
+Solve:
+- Setup the two generators: 
+    prevA, prevB are from input (starting numbers),
+    multA, multB = if 0 not checked, if non-zero, filters generator results to only be multiples of this number 
+    factorA, factorB = fixed for A = 16807, B = 48271
+- Run for the specified number of steps, get the next output from genA and genB:
+    next number = (previous * factor) % 2147483647; remember previous for next result 
+    if mult is non-zero, yield only values that are cleanly divided by mult
+- Count the number of matched responses: a and b match if the last 16 digits of their binary representations are the same
+- For Part 1, dont use multiples, and run for 40,000,000 steps 
+- For Part 2, use multA=4, multB=8, and run for 5,000,000 steps
+'''
