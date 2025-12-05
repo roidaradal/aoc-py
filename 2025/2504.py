e3d572c62e5a8e6948021fa52f054b92b7c728ea
roidaradal <roidaradal@gmail.com>
2025-12-05 14:07:01 +0800

    25.05 Py

--- 2025/2504.py
+++ 2025/2504.py
@@ -11,63 +11,38 @@ def data(full: bool) -> PaperGrid:
     return [fn(line) for line in readLines(25, 4, full)]
 
 def solve() -> Solution:
-    return newSolution(part1(), part2())
-
-def part1() -> int:
     grid = data(full=True)
     bounds = getBounds(grid)
-    rows, cols = bounds 
-    count = 0
-    for row in range(rows):
-        for col in range(cols):
-            if not grid[row][col]: continue # skip not paper
-            activeNeighbors = [(r,c) for r,c in surround8((row,col)) if insideBounds((r,c),bounds) and grid[r][c]]
-            if len(activeNeighbors) < 4:
-                count += 1
-    return count
-
-def part2() -> int:
-    grid = data(full=True)
-    bounds = getBounds(grid)
-    rows, cols = bounds 
-    total = 0
+    total1, total2 = 0, 0
     while True:
         grid2: PaperGrid = []
         count = 0
-        for row in range(rows):
-            line: list[bool] = []
-            for col in range(cols):
-                if not grid[row][col]: 
-                    line.append(False)
-                    continue # skip not paper
-
-                activeNeighbors = [(r,c) for r,c in surround8((row,col)) if insideBounds((r,c),bounds) and grid[r][c]]
-                if len(activeNeighbors) < 4:
-                    line.append(False)
+        for row, line in enumerate(grid):
+            line2 = line[:]
+            for col, paper in enumerate(line):
+                if not paper: continue # skip not paper 
+                # check neighbors that are inside bounds and are paper
+                paperNeighbors = len([True for r,c in surround8((row,col)) if insideBounds((r,c), bounds) and grid[r][c]])
+                if paperNeighbors < 4:
+                    line2[col] = False 
                     count += 1
-                else:
-                    line.append(True)
-            grid2.append(line)
-        total += count
+            grid2.append(line2)
+        if total1 == 0: total1 = count
+        total2 += count
         if count == 0: break
         grid = grid2
 
-    return total
+    return newSolution(total1, total2)
 
 if __name__ == '__main__':
     do(solve, 25, 4)
 
 '''
-Part1:
-- Go through each cell in the grid
-- For each paper cell (True), count how many paper neighbors it has (surround8)
-- If count < 4, increment the count
-- Return the count of paper cells with < 4 paper neighbors
-
-Part2:
-- Find the papers to destroy with the process similar to Part 1 
-- Remove the papers and form the next grid
-- Count the papers destroyed in this round
-- Repeat on the resulting grid until no more papers are destroyed
-- Return total count of papers destroyed
+Solve:
+- Repeatedly update the paper grid by removing the papers with < 4 paper neighbors
+- Go through each paper cell in the grid and count their paper neighbors
+- Those with < 4 paper neighbors are removed in the resulting grid
+- Stop if there are no more papers to remove from the current grid
+- For Part 1, output the number of papers removed in the first round
+- For Part 2, output the total number of papers removed after all rounds
 '''
---
+++ 2025/2505.py
@@ -0,0 +1,46 @@
+# Advent of Code 2025 Day 05
+# John Roy Daradal 
+
+from aoc import *
+
+def data(full: bool) -> tuple[list[int2], list[int]]:
+    ranges: list[int2] = []
+    ingredients: list[int] = []
+    inPart2 = False
+    for line in readLines(25, 5, full):
+        if line == '':
+            inPart2 = True 
+        elif inPart2:
+            ingredients.append(int(line))
+        else:
+            ranges.append(toInt2(line, '-'))
+    return ranges, ingredients
+
+def solve() -> Solution:
+    ranges, ingredients = data(full=True)
+    ranges = mergeRanges(ranges)
+
+    # Part 1
+    count1 = 0
+    for ingredient in ingredients:
+        if any(first <= ingredient <= last for first,last in ranges):
+            count1 += 1
+
+    # Part 2
+    count2 = sum(last-first+1 for first,last in ranges)
+      
+    return newSolution(count1, count2)
+
+if __name__ == '__main__':
+    do(solve, 25, 5)
+
+'''
+Part1:
+- Go through each ingredient 
+- Check if any of the ranges allows the ingredient to be in range
+- Output the count of ingredients that fall in one of the ranges
+
+Part2:
+- Merge the ranges to eliminate subsets and overlaps 
+- Sum up the sizes of the resulting ranges
+'''
